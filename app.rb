6dccf1c08dbc933c5aa4f8a35f44b77338078dd5
swapman972 <jordan.melidor@hotmail.fr>
2020-04-19 15:46:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+      end
+
+    get '/name' do
+        "My name is ___!"
+    end
+    
+    get '/hometown' do
+        "My hometown is ___!"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is ___"
+    end
 end
